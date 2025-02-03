f440ebfa5ca42101b089355428ea8df02de90b1b
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-04 01:13:56 +0530

    Initial commit

---
+++ viviki/src/main/java/com/ecom/viviki/VivikiApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.viviki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivikiApplication.class, args);
+	}
+
+}
---
+++ viviki/src/test/java/com/ecom/viviki/VivikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.viviki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
