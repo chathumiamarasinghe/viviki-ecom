549b603391f46c72b55ce2b247d67c7557930804
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-06 17:13:09 +0530

    Create UserRepo.java

---
+++ viviki/src/main/java/com/ecom/viviki/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.ecom.viviki.repository;
+
+import com.ecom.viviki.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    Optional<User> findEmail(String email);
+}
