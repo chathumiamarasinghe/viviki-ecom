b82e66f22a637545bca2af840e4a96a32ab23207
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-06 17:13:01 +0530

    Create OrderItemRepo.java

---
+++ viviki/src/main/java/com/ecom/viviki/repository/OrderItemRepo.java
@@ -0,0 +1,8 @@
+package com.ecom.viviki.repository;
+
+import com.ecom.viviki.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface OrderItemRepo extends JpaRepository<OrderItem, Long>, JpaSpecificationExecutor<OrderItem> {
+}
