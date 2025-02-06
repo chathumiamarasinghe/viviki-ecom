bfda854329e1471f9b62ccf154339e6600a63bbd
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-06 17:12:56 +0530

    Create ProductRepo.java

---
+++ viviki/src/main/java/com/ecom/viviki/repository/ProductRepo.java
@@ -0,0 +1,12 @@
+package com.ecom.viviki.repository;
+
+import com.ecom.viviki.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+public interface ProductRepo extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategoryId(Long categoryId);
+
+    List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+}
