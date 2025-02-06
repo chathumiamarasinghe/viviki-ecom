676d95df7863382f4731c28977382ccbc7fe2cb2
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-06 12:49:08 +0530

    Create LoginRequest.java

---
+++ viviki/src/main/java/com/ecom/viviki/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.ecom.viviki.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
