cbe6700cdac9d694d307370411ecb52d528cbd7c
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-13 00:05:44 +0530

    rename correctly

--- viviki/src/main/java/com/ecom/viviki/security/CustomUserDetailsService.java
+++ viviki/src/main/java/com/ecom/viviki/security/CustomUserDetailsService.java
@@ -1,16 +1,13 @@
 package com.ecom.viviki.security;
 
-import com.ecom.viviki.dto.UserDto;
 import com.ecom.viviki.entity.User;
-import com.ecom.viviki.execption.NotFoundException;
+import com.ecom.viviki.exception.NotFoundException;
 import com.ecom.viviki.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import static ch.qos.logback.classic.spi.ThrowableProxyVO.build;
 
 @Service
 @RequiredArgsConstructor
