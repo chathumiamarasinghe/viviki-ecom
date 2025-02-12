2d9cc4abfc44858270d673fff30a2ff3766a5566
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-13 00:06:16 +0530

    add missing lombok

--- viviki/src/main/java/com/ecom/viviki/dto/LoginRequest.java
+++ viviki/src/main/java/com/ecom/viviki/dto/LoginRequest.java
@@ -1,12 +1,13 @@
 package com.ecom.viviki.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class LoginRequest {
 
     @NotBlank(message = "Email is required")
     private String email;
-
     @NotBlank(message = "Password is required")
     private String password;
 }
