3983bd9535d7d8a8bd8dca50bc7a3ffa4ce76cd8
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-16 00:36:40 +0530

    Create OrderItemServiceImpl.java

---
+++ viviki/src/main/java/com/ecom/viviki/service/impl/OrderItemServiceImpl.java
@@ -0,0 +1,122 @@
+package com.ecom.viviki.service.impl;
+
+import com.ecom.viviki.dto.OrderItemDto;
+import com.ecom.viviki.dto.OrderRequest;
+import com.ecom.viviki.dto.Response;
+import com.ecom.viviki.entity.Order;
+import com.ecom.viviki.entity.OrderItem;
+import com.ecom.viviki.entity.Product;
+import com.ecom.viviki.entity.User;
+import com.ecom.viviki.enums.OrderStatus;
+import com.ecom.viviki.exception.NotFoundException;
+import com.ecom.viviki.mapper.EntityDtoMapper;
+import com.ecom.viviki.repository.OrderItemRepo;
+import com.ecom.viviki.repository.OrderRepo;
+import com.ecom.viviki.repository.ProductRepo;
+import com.ecom.viviki.service.interf.OrderItemService;
+import com.ecom.viviki.service.interf.UserService;
+import com.ecom.viviki.specification.OrderItemSpecification;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderItemServiceImpl implements OrderItemService {
+
+
+    private final OrderRepo orderRepo;
+    private final OrderItemRepo orderItemRepo;
+    private final ProductRepo productRepo;
+    private final UserService userService;
+    private final EntityDtoMapper entityDtoMapper;
+
+
+    @Override
+    public Response placeOrder(OrderRequest orderRequest) {
+
+        User user = userService.getLoginUser();
+
+
+        List<OrderItem> orderItems = orderRequest.getItems().stream().map(orderItemRequest -> {
+            Product product = productRepo.findById(orderItemRequest.getProductId())
+                    .orElseThrow(()-> new NotFoundException("Product Not Found"));
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(product);
+            orderItem.setQuantity(orderItemRequest.getQuantity());
+            orderItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(orderItemRequest.getQuantity())));
+            orderItem.setStatus(OrderStatus.PENDING);
+            orderItem.setUser(user);
+            return orderItem;
+
+        }).collect(Collectors.toList());
+
+
+        BigDecimal totalPrice = orderRequest.getTotalPrice() != null && orderRequest.getTotalPrice().compareTo(BigDecimal.ZERO) > 0
+                ? orderRequest.getTotalPrice()
+                : orderItems.stream().map(OrderItem::getPrice).reduce(BigDecimal.ZERO, BigDecimal::add);
+
+
+        Order order = new Order();
+        order.setOrderItemList(orderItems);
+        order.setTotalPrice(totalPrice);
+
+
+        orderItems.forEach(orderItem -> orderItem.setOrder(order));
+
+        orderRepo.save(order);
+
+        return Response.builder()
+                .status(200)
+                .message("Order was successfully placed")
+                .build();
+
+    }
+
+    @Override
+    public Response updateOrderItemStatus(Long orderItemId, String status) {
+        OrderItem orderItem = orderItemRepo.findById(orderItemId)
+                .orElseThrow(()-> new NotFoundException("Order Item not found"));
+
+        orderItem.setStatus(OrderStatus.valueOf(status.toUpperCase()));
+        orderItemRepo.save(orderItem);
+        return Response.builder()
+                .status(200)
+                .message("Order status updated successfully")
+                .build();
+    }
+
+    @Override
+    public Response filterOrderItems(OrderStatus status, LocalDateTime startDate, LocalDateTime endDate, Long itemId, Pageable pageable) {
+        Specification<OrderItem> spec = Specification.where(OrderItemSpecification.hasStatus(status))
+                .and(OrderItemSpecification.createdBetween(startDate, endDate))
+                .and(OrderItemSpecification.hasItemId(itemId));
+
+        Page<OrderItem> orderItemPage = orderItemRepo.findAll(spec, pageable);
+
+        if (orderItemPage.isEmpty()){
+            throw new NotFoundException("No Order Found");
+        }
+        List<OrderItemDto> orderItemDtos = orderItemPage.getContent().stream()
+                .map(entityDtoMapper::mapOrderItemToDtoPlusProductAndUser)
+                .collect(Collectors.toList());
+
+        return Response.builder()
+                .status(200)
+                .orderItemList(orderItemDtos)
+                .totalPage(orderItemPage.getTotalPages())
+                .totalElement(orderItemPage.getTotalElements())
+                .build();
+    }
+
+}
