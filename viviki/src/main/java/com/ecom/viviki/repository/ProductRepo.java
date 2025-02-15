0fcdd97ff1764690202bf1fc80bf991651b87610
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-16 00:36:29 +0530

    change int to long

--- viviki/src/main/java/com/ecom/viviki/repository/ProductRepo.java
+++ viviki/src/main/java/com/ecom/viviki/repository/ProductRepo.java
@@ -4,9 +4,8 @@ import com.ecom.viviki.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+
 public interface ProductRepo extends JpaRepository<Product, Long> {
-
     List<Product> findByCategoryId(Long categoryId);
-
     List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
 }
