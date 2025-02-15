e96f9867536252c76a48cbb955232614b32a19f7
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-16 00:35:46 +0530

    fix mismatch issue

--- viviki/src/main/java/com/ecom/viviki/security/JwtUtils.java
+++ viviki/src/main/java/com/ecom/viviki/security/JwtUtils.java
@@ -22,7 +22,7 @@ public class JwtUtils {
     private static final long EXPIRATION_TIME_IN_MILLISEC = 1000L * 60L *60L *24L * 30L * 6L;
     private SecretKey key;
 
-    @Value("${secreteJwtString}")
+    @Value("${secretJwtString}")
     private String secreteJwtString;
 
     @PostConstruct
