c4909ac65cd3419687dafe7f7c5edf15599d3a98
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-26 08:29:46 +0530

    route configurations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { BrowserRouter, Route, Navigate, Routes, } from 'react-router-dom';
+import Navbar from './component/common/Navbar';
+import Footer from './component/common/Footer';
+import {CartProvider} from './component/context/CartContext';
+import Home from './component/pages/Home';
+
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+   <BrowserRouter>
+   <CartProvider>
+    <Navbar/>
+    <Routes>
+      {/* OUR ROUTES */}
+      <Route exact path='/' element={<Home/>}/>
+    </Routes>
+    <Footer/>
+   </CartProvider>
+   </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
