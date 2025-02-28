0a928484cac6630a12c14b0ef2139e79fa095a90
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-02-28 22:37:08 +0530

    add new roting paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from './component/common/Navbar';
 import Footer from './component/common/Footer';
 import {CartProvider} from './component/context/CartContext';
 import Home from './component/pages/Home';
+import ProductDetailsPage from './component/pages/ProductDetailsPage';
+import CategoryListPage from './component/pages/CategoryListPage';
+import CategoryProductsPage from './component/pages/CategoryProductsPage';
 
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
     <Routes>
       {/* OUR ROUTES */}
       <Route exact path='/' element={<Home/>}/>
+      <Route path='/product/:productId' element={<ProductDetailsPage/>} />
+      <Route path='/categories' element={<CategoryListPage/>}/>
+      <Route path='/category/:categoryId' element={<CategoryProductsPage/>} />
     </Routes>
     <Footer/>
    </CartProvider>
