2c5f5d8e49c22e672f05023dec698a28036bd2c2
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-04-25 19:06:08 +0530

    Add MaterialService.java interface

--- src/main/java/com/second/Eccormerce/service/interf/MaterialService.java
+++ src/main/java/com/second/Eccormerce/service/interf/MaterialService.java
@@ -1,2 +1,14 @@
-package com.second.Eccormerce.service.interf;public interface MaterialService {
+package com.second.Eccormerce.service.interf;
+
+import com.second.Eccormerce.dto.Response;
+
+
+public interface MaterialService {
+
+    Response createMaterial(String name, String description, Integer quantity);
+    Response updateMaterial(Long materialId, String name, String description, Integer quantity);
+    Response deleteMaterial(Long productId);
+    Response getMaterialById(Long productId);
+    Response getAllMaterials();
+    Response searchMaterial(String searchValue);
 }
