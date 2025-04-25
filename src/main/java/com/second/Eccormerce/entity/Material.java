4ccf32a38b60c52eefa96c9575ae27c6d6942a5f
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-04-25 19:06:06 +0530

    Add Material.java entity

--- src/main/java/com/second/Eccormerce/entity/Material.java
+++ src/main/java/com/second/Eccormerce/entity/Material.java
@@ -1,2 +1,48 @@
-package com.second.Eccormerce.entity;public class Material {
+package com.second.Eccormerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Data
+@Entity
+@Table(name = "materials")
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Integer quantity;
+
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Material material = (Material) o;
+        return Objects.equals(id, material.id) &&
+                Objects.equals(name, material.name) &&
+                Objects.equals(description, material.description) &&
+                Objects.equals(quantity, material.quantity);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, quantity);
+    }
+
+
+    public boolean canEqual(Object other) {
+        return other instanceof Material;
+    }
 }
