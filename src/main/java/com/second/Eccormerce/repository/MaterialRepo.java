8283e793f3acc788280be74139cac5fd0633030d
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-04-25 19:06:07 +0530

    Add MaterialRepo.java repository

--- src/main/java/com/second/Eccormerce/repository/MaterialRepo.java
+++ src/main/java/com/second/Eccormerce/repository/MaterialRepo.java
@@ -1,2 +1,10 @@
-package com.second.Eccormerce.repository;public interface MaterialRepo {
+package com.second.Eccormerce.repository;
+
+import com.second.Eccormerce.entity.Material;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MaterialRepo extends JpaRepository<Material, Long> {
+    List<Material> findByNameContainingOrDescriptionContaining(String name, String description);
 }
