dd11a1aaef1a5b1d71b2370acef3b694ff0e532c
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-05-10 11:54:33 +0530

    add admin panel routes

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js'; // Import Stripe's loadStripe function
+import { loadStripe } from '@stripe/stripe-js';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './component/common/Navbar';
 import Footer from './component/common/Footer';
@@ -39,6 +39,7 @@ import DeliveryPersonPage from './component/deliveryperson/DeliveryPerson';
 import InventoryManagerPage from './component/inventorymanager/InventoryManager';
 import AdminUserPage from './component/admin/AdminUserPage';
 import AddUser from './component/admin/AddUser';
+import AdminLayout from "./component/admin/AdminLayout";
 
 // Stripe public key
 const stripePromise = loadStripe('pk_test_51R20BrQuQy8aXxhAx1DlRKKigcryCTHcdVG6Re0qtmEb6Gzs0zf83Ibd5LE8IPy3qB3BDEaIH8mCXFc9hOwLIB4W0015O3VX9l'); // Replace with your Stripe public key
@@ -50,7 +51,7 @@ function App() {
         <Navbar/>
         <Elements stripe={stripePromise}>
           <Routes>
-            {/* OUR ROUTES */}
+            {/* Public Routes */}
             <Route exact path='/' element={<Home/>}/>
             <Route path='/product/:productId' element={<ProductDetailsPage/>} />
             <Route path='/categories' element={<CategoryListPage/>}/>
@@ -63,37 +64,33 @@ function App() {
             <Route path='/CategoryChart' element={<CategoryChart/>}/>
             <Route path='/OrderChart' element={<OrderChart/>}/>
             <Route path='/category-section' element={<CategorySection />} />
-
             <Route path='/profile' element={<ProfilePage/>} />
             <Route path='/add-address' element={<AddressPage/>} />
             <Route path='/edit-address' element={<AddressPage/>} />
-
-            {/* ADMIN ROUTES */}
-            <Route path='/admin' element={<AdminPage/>} />
-            <Route path='/admin/users' element={<AdminUserPage/>} />
-            <Route path='/admin/add-user' element={<AddUser/>} />
             
-            {/* INVENTORY MANAGER & ADMIN ROUTES */}
-            <Route path='/admin/add-product' element={<AddProductPage />} />
-            <Route path='/admin/edit-product/:productId' element={<EditProductPage />} />
-            <Route path='/admin/products' element={<AdminProductPage />} />
-            <Route path='/admin/add-category' element={<AddCategory />} />
-            <Route path='/admin/categories' element={<AdminCategoryPage />} />
-            <Route path='/admin/edit-category/:categoryId' element={<EditCategory />} />
-            <Route path='/admin/materials' element={<AdminMaterialPage />} />
-            <Route path='/admin/add-material' element={<AddMaterialPage />} />
-            <Route path='/admin/edit-material/:materialId' element={<EditMaterialPage />} />
-            <Route path='/admin/materialTypes' element={<AdminMaterialTypePage />} />
-            <Route path='/admin/add-materialType' element={<AddMaterialTypePage />} />
-            <Route path='/admin/edit-materialType/:materialTypeId' element={<EditMaterialTypePage />} />
+            {/* Admin Routes */}
+            <Route path='/admin' element={<AdminLayout />}>
+              <Route index element={<AdminPage />} />
+              <Route path="users" element={<AdminUserPage />} />
+              <Route path="add-user" element={<AddUser />} />
+              <Route path="add-product" element={<AddProductPage />} />
+              <Route path="edit-product/:productId" element={<EditProductPage />} />
+              <Route path="products" element={<AdminProductPage />} />
+              <Route path="add-category" element={<AddCategory />} />
+              <Route path="categories" element={<AdminCategoryPage />} />
+              <Route path="edit-category/:categoryId" element={<EditCategory />} />
+              <Route path="materials" element={<AdminMaterialPage />} />
+              <Route path="add-material" element={<AddMaterialPage />} />
+              <Route path="edit-material/:materialId" element={<EditMaterialPage />} />
+              <Route path="materialTypes" element={<AdminMaterialTypePage />} />
+              <Route path="add-materialType" element={<AddMaterialTypePage />} />
+              <Route path="edit-materialType/:materialTypeId" element={<EditMaterialTypePage />} />
+              <Route path="orders" element={<AdminOrdersPage />} />
+              <Route path="order-details/:itemId" element={<AdminOrderDetailsPage />} />
+            </Route>
 
-            <Route path='/admin/orders' element={<AdminOrdersPage/>} />
-            <Route path='/admin/order-details/:itemId' element={<AdminOrderDetailsPage/>} />
-
-            {/* INVENTORY MANAGER ROUTES */}
+            {/* Manager & Delivery Person Routes */}
             <Route path='/manager' element={<InventoryManagerPage/>} />
-            
-            {/* DELIVERY PERSON ROUTES */}
             <Route path='/delivery' element={<DeliveryPersonPage/>} />
             <Route path='/delivery/orders' element={<AdminOrdersPage />} />
             <Route path='/delivery/order-details/:itemId' element={<AdminOrderDetailsPage />} />
