7ba7a7848c17d00619f9988db3d00721802379d9
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-05-17 09:13:18 +0530

    Update UserRepo: add custom query methods

--- src/main/java/com/second/Eccormerce/repository/UserRepo.java
+++ src/main/java/com/second/Eccormerce/repository/UserRepo.java
@@ -3,9 +3,11 @@ package com.second.Eccormerce.repository;
 import com.second.Eccormerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
 }
